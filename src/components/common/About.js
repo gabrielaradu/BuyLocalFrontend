0e2715dcb73def4bb3dcbb66c4bdfc2d2e4d0c46
Gabi <gabrielaradu.developer@gmail.com>
2020-04-16 16:34:14 +0300

    Fix warnings and take off shadow because it looks bad.

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class About extends Component {
     render() {
         return (
-            <div className="container mx-auto bg-white py-8 border-t border-gray-400 shadow-md">
+            <div className="container mx-auto bg-white py-8 border-t border-gray-400">
                 <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="lg:text-center">
                         <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Twilio Hackathon</p>
@@ -25,7 +25,7 @@ class About extends Component {
                                             className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                                             <svg className="h-6 w-6" stroke="currentColor" fill="none"
                                                  viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                       d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
                                             </svg>
                                         </div>
@@ -45,7 +45,7 @@ class About extends Component {
                                             className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                                             <svg className="h-6 w-6" stroke="currentColor" fill="none"
                                                  viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                       d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
                                             </svg>
                                         </div>
@@ -65,7 +65,7 @@ class About extends Component {
                                             className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                                             <svg className="h-6 w-6" stroke="currentColor" fill="none"
                                                  viewBox="0 0 24 24">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                       d="M13 10V3L4 14h7v7l9-11h-7z"/>
                                             </svg>
                                         </div>
